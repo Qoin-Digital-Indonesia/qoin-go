ea688b6f4f08b42f68d97d3d3058ca04e6b807b6
Girein <giovanni.reinard@gmail.com>
2020-11-10 22:22:44 +0700

    update signature

--- signature.go
+++ signature.go
@@ -15,78 +15,61 @@ import (
 
 // GenerateSignature returns generated RSA signature
 func GenerateSignature(text string) string {
-	signer, err := loadPrivateKey("private.pem")
+	signer, err := loadPrivateKey()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "signer is damaged: %s", err)
+		fmt.Fprintf(os.Stderr, "Signer is damaged: %s", err)
 	}
 
-	toSign := "date: Thu, 05 Jan 2012 21:31:40 GMT"
-
-	signed, err := signer.Sign([]byte(toSign))
+	signed, err := signer.Sign([]byte(text))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "could not sign request: %s", err)
+		fmt.Fprintf(os.Stderr, "Could not sign request: %s", err)
 	}
-	sig := base64.StdEncoding.EncodeToString(signed)
-	fmt.Printf("Signature: %v\n", sig)
 
-	return sig
+	return base64.StdEncoding.EncodeToString(signed)
 }
 
-// loadPrivateKey loads an parses a PEM encoded private key file.
-func loadPrivateKey(path string) (Signer, error) {
-	return parsePrivateKey([]byte(`-----BEGIN RSA PRIVATE KEY-----
-MIICXgIBAAKBgQDCFENGw33yGihy92pDjZQhl0C36rPJj+CvfSC8+q28hxA161QF
-NUd13wuCTUcq0Qd2qsBe/2hFyc2DCJJg0h1L78+6Z4UMR7EOcpfdUE9Hf3m/hs+F
-UR45uBJeDK1HSFHD8bHKD6kv8FPGfJTotc+2xjJwoYi+1hqp1fIekaxsyQIDAQAB
-AoGBAJR8ZkCUvx5kzv+utdl7T5MnordT1TvoXXJGXK7ZZ+UuvMNUCdN2QPc4sBiA
-QWvLw1cSKt5DsKZ8UETpYPy8pPYnnDEz2dDYiaew9+xEpubyeW2oH4Zx71wqBtOK
-kqwrXa/pzdpiucRRjk6vE6YY7EBBs/g7uanVpGibOVAEsqH1AkEA7DkjVH28WDUg
-f1nqvfn2Kj6CT7nIcE3jGJsZZ7zlZmBmHFDONMLUrXR/Zm3pR5m0tCmBqa5RK95u
-412jt1dPIwJBANJT3v8pnkth48bQo/fKel6uEYyboRtA5/uHuHkZ6FQF7OUkGogc
-mSJluOdc5t6hI1VsLn0QZEjQZMEOWr+wKSMCQQCC4kXJEsHAve77oP6HtG/IiEn7
-kpyUXRNvFsDE0czpJJBvL/aRFUJxuRK91jhjC68sA7NsKMGg5OXb5I5Jj36xAkEA
-gIT7aFOYBFwGgQAQkWNKLvySgKbAZRTeLBacpHMuQdl1DfdntvAyqpAZ0lY0RKmW
-G6aFKaqQfOXKCyWoUiVknQJAXrlgySFci/2ueKlIE1QqIiLSZ8V8OlpFLRnb1pzI
-7U1yQXnTAEFYM560yJlzUpOb1V4cScGd365tiSMvxLOvTA==
------END RSA PRIVATE KEY-----`))
+func loadPrivateKey() (Signer, error) {
+	return parsePrivateKey([]byte(privateKey))
 }
 
-// parsePublicKey parses a PEM encoded private key.
 func parsePrivateKey(pemBytes []byte) (Signer, error) {
 	block, _ := pem.Decode(pemBytes)
 	if block == nil {
-		return nil, errors.New("ssh: no key found")
+		return nil, errors.New("SSH: No key found")
 	}
 
-	var rawkey interface{}
+	var rawKey interface{}
+
 	switch block.Type {
 	case "RSA PRIVATE KEY":
 		rsa, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 		if err != nil {
 			return nil, err
 		}
-		rawkey = rsa
+
+		rawKey = rsa
 	default:
-		return nil, fmt.Errorf("ssh: unsupported key type %q", block.Type)
+		return nil, fmt.Errorf("SSH: Unsupported key type %q", block.Type)
 	}
-	return newSignerFromKey(rawkey)
+
+	return newSignerFromKey(rawKey)
 }
 
-// A Signer is can create signatures that verify against a public key.
+// Signer can create signatures that verify against a public key.
 type Signer interface {
-	// Sign returns raw signature for the given data. This method
-	// will apply the hash specified for the keytype to the data.
 	Sign(data []byte) ([]byte, error)
 }
 
 func newSignerFromKey(k interface{}) (Signer, error) {
 	var sshKey Signer
+
 	switch t := k.(type) {
 	case *rsa.PrivateKey:
 		sshKey = &rsaPrivateKey{t}
 	default:
-		return nil, fmt.Errorf("ssh: unsupported key type %T", k)
+		return nil, fmt.Errorf("SSH: Unsupported key type %T", k)
 	}
+
 	return sshKey, nil
 }
 
@@ -94,10 +77,10 @@ type rsaPrivateKey struct {
 	*rsa.PrivateKey
 }
 
-// Sign signs data with rsa-sha256
 func (r *rsaPrivateKey) Sign(data []byte) ([]byte, error) {
 	h := sha256.New()
 	h.Write(data)
 	d := h.Sum(nil)
+
 	return rsa.SignPKCS1v15(rand.Reader, r.PrivateKey, crypto.SHA256, d)
 }
