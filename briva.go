be6551adb8c55f487aef08369d25fd172c77e5d3
Girein <giovanni.reinard@gmail.com>
2020-11-11 00:48:20 +0700

    update briva

--- briva.go
+++ briva.go
@@ -3,6 +3,7 @@ package qoin
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 // BriVaCreateOrder returns BRI VA Create Order API endpoint
@@ -16,13 +17,17 @@ func BriVaCreateOrder(body map[string]interface{}) string {
 		endpoint = "https://dev-sandbox-apipg.qoin.id/sandbox/bri/order"
 	}
 
-	body["SecretKey"] = secretKey
-	payload, err := json.Marshal(body)
+	jsonBody, err := json.Marshal(body)
 	if err != nil {
 		fmt.Println("[BRI VA Create Order] JSON encode body got error:", err)
 	}
-	fmt.Println(string(payload))
-	// signature := generateSignature(string(payload))
+
+	bodyLastIndex := strings.LastIndex(string(jsonBody), "}")
+	trimmedBody := string(jsonBody)[:bodyLastIndex]
+	payload := trimmedBody + ",\"SecretKey\":\"" + secretKey + "\"}"
+	signature := generateSignature(payload)
+	fmt.Println(payload)
+	fmt.Println(signature)
 
 	return endpoint
 }
