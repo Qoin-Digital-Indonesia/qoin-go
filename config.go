a1b67de108687b24872f8a4c24e8fdc7a6f02b96
Girein <giovanni.reinard@gmail.com>
2020-11-10 13:09:38 +0700

    update config: set private key, set secret key, set reference number

--- config.go
+++ config.go
@@ -1,6 +1,11 @@
 package qoin
 
-var isProduction bool
+var (
+	isProduction    bool
+	privateKey      string
+	secretKey       string
+	referenceNumber string
+)
 
 // SetEnvironment set the API environment
 func SetEnvironment(environment string) {
@@ -10,4 +15,19 @@ func SetEnvironment(environment string) {
 	default:
 		isProduction = false
 	}
+}
+
+// SetPrivateKey set the private key for generate signature
+func SetPrivateKey(privKey string) {
+	privateKey = privKey
+}
+
+// SetSecretKey set the secret key for generate signature
+func SetSecretKey(secKey string) {
+	secretKey = secKey
+}
+
+// SetReferenceNumber set the reference number for generate signature
+func SetReferenceNumber(refNumber string) {
+	referenceNumber = refNumber
 }
