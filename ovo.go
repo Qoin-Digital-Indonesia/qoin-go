abca7c4d85e567735ec39c6f29ade2ff6b62e082
Girein <giovanni.reinard@gmail.com>
2020-11-12 15:22:12 +0700

    add ovo: create order

---
+++ ovo.go
@@ -0,0 +1,61 @@
+package qoin
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// OvoCreateOrder returns the response from OVO Create Order API
+func OvoCreateOrder(body map[string]interface{}) map[string]interface{} {
+	var endpoint string
+
+	switch isProduction {
+	case true:
+		endpoint = "https://dev-apipg.qoin.id/espay-order"
+	default:
+		endpoint = "https://dev-sandbox-apipg.qoin.id/sandbox/wallet/order"
+	}
+
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		fmt.Println("[OVO Create Order] JSON encode body got error:", err)
+	}
+
+	payload, err := json.Marshal(map[string]interface{}{
+		"ReferenceNo": body["ReferenceNo"],
+		"ReqTime":     body["ReqTime"],
+		"SecretKey":   secretKey,
+	})
+	if err != nil {
+		fmt.Println("[OVO Create Order] JSON encode payload got error:", err)
+	}
+
+	signature := generateSignature(string(payload))
+
+	client := &http.Client{}
+	request, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		fmt.Println("[OVO Create Order] Create HTTP request got error:", err)
+	}
+
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Signature", signature)
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("[OVO Create Order] Send HTTP request got error:", err)
+	}
+
+	defer response.Body.Close()
+
+	var result map[string]interface{}
+	err = json.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		fmt.Println("[OVO Create Order] JSON decode response got error:", err)
+	}
+
+	return result
+}
